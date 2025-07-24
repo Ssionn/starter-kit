a1938d3fb352772e4321b8fcea5e92c721de4f21
Ssionn <casper.kizewski.developer@gmail.com>
2025-07-25 00:53:15 +0200

    WIP

--- app/Http/Controllers/Settings/SecurityController.php
+++ app/Http/Controllers/Settings/SecurityController.php
@@ -37,21 +37,9 @@ class SecurityController extends Controller
     public function generateQr(): JsonResponse
     {
         $secret = Google2FA::generateSecretKey();
-        $cypher = 'aes-128-gcm';
-        $key = config('app.otp_key');
-        $options = 0;
-        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-gcm'));
-        $tag = config('app.otp_tag');
 
         auth()->user()->update([
-            'google2fa_secret' => openssl_encrypt(
-                $secret,
-                $cypher,
-                $key,
-                $options,
-                $iv,
-                $tag
-            )
+            'google2fa_secret' => $secret
         ]);
 
         $svg = Google2FA::getQRCodeInline(
@@ -68,20 +56,8 @@ class SecurityController extends Controller
     public function enable2fa(Request $request)
     {
         $request->validate(['otp' => 'required|digits:6']);
-        $cypher = 'aes-128-gcm';
-        $key = config('app.otp_key');
-        $options = 0;
-        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-gcm'));
-        $tag = config('app.otp_tag');
 
-        $secret = openssl_decrypt(
-            auth()->user()->google2fa_secret,
-            $cypher,
-            $key,
-            $options,
-            $iv,
-            $tag
-        );
+        $secret = auth()->user()->google2fa_secret;
 
         if (! Google2FA::verifyKey($secret, $request->otp)) {
           return response()->json(['error' => 'Invalid code'], 422);
@@ -91,6 +67,6 @@ class SecurityController extends Controller
             'google2fa_enabled' => true
         ]);
 
-        return response()->json(['success' => true]);
+        return back()->with('status', 'OTP has been enabled');
   }
 }
--- resources/views/settings/partials/security/two-factor-authentication.blade.php
+++ resources/views/settings/partials/security/two-factor-authentication.blade.php
@@ -3,29 +3,30 @@
     <div class="flex items-center space-x-2">
         <span class="text-xl font-semibold">{{ __('Two factor authentication') }}</span>
         @if (auth()->user()->google2fa_enabled)
-            <span class="text-sm font-semibold bg-emerald-400 dark:bg-emerald-800 px-4 py-1 rounded-md">
+            <span class="text-sm font-semibold bg-emerald-400 dark:bg-emerald-800 px-4 py-0.5 rounded-md">
                 {{ __('Activated') }}
             </span>
         @else
-            <span class="text-sm font-semibold bg-red-400 dark:bg-red-800 px-2 py-1 rounded-md">
+            <span class="text-sm font-semibold bg-red-400 dark:bg-red-800 px-2 py-0.5 rounded-md">
                 {{ __('Not activated') }}
             </span>
         @endif
     </div>
     @if (!auth()->user()->google2fa_enabled)
         <div x-data="twoFa()" x-init="() => {}">
-            <x-button x-show="!showQr && !success" @click="generate()">{{ __('Enable one-time-password') }}</x-button>
+            <x-button class="mt-4" x-show="!showQr && !success"
+                @click="generate()">{{ __('Enable one-time-password') }}</x-button>
 
-            <template x-if="showQr">
-                <div class="mt-4">
-                    <img :src="qrCode" alt="QR Code" class="mb-4">
-                    <form @submit.prevent="verify()">
-                        <input x-model="otp" type="text" placeholder="123456" class="border p-2 mr-2">
-                        <button class="px-3 py-2 bg-green-600 text-white">
-                            Verify
-                        </button>
-                    </form>
-                    <p x-text="error" class="text-red-600 mt-2"></p>
+            <template x-if="showQr" class="w-full">
+                <div class="flex flex-col items-center sm:flex-row mt-4 w-full">
+                    <img :src="qrCode" alt="QR Code" class="w-32 h-32">
+                    <div class="ml-0 sm:ml-4 mt-2 sm:mt-0">
+                        <form @submit.prevent="verify()">
+                            <x-forms.input label="Verify" name="otp" x-model="otp" placeholder="123456"
+                                class="border p-2 mr-2" />
+                        </form>
+                        <p x-text="error" class="text-red-600 mt-2">{{ __('Please fill in the correct otp code') }}</p>
+                    </div>
                 </div>
             </template>
         </div>
